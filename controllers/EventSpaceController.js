30a36283ec47f4607dffaee99de5ab55e13fe6cd
elisaul batista <elisaulbatista@pursuit.org>
2024-01-25 17:49:11 -0500

    added queries

--- controllers/EventSpaceController.js
+++ controllers/EventSpaceController.js
@@ -1,65 +1,74 @@
-const express = require("express")
+const express = require("express");
+
+const events = express.Router();
 
 
-const events = express.Router()
+const {
+  getAllEvents,
+  getOneEvent,
+  deleteEvent,
+  updateEvent,
+  createEvent,
+} = require("../queries/events.js");
 
-const bookingsController = require("./bookingsController.js")
-events.use("/:event_id/bookings", bookingsController)
+
+
+const bookingsController = require("./bookingsController.js");
+events.use("/:event_id/bookings", bookingsController);
 
 events.get("/", async (req, res) => {
-    const allEvents = await getAllEvents();
-    console.log(allEvents)
-    try {
-        if(allEvents[0]) {
-            res.status(200).json({success:true, data:{payload: allEvents}
-            }
-            )
-        }
-    } catch(err) {
-        console.log(err)
-        res.status(404).json({success:false, data:{error: err}});
+  const allEvents = await getAllEvents();
+  console.log(allEvents);
+  try {
+    if (allEvents[0]) {
+      res.status(200).json({ success: true, data: { payload: allEvents } });
     }
+  } catch (err) {
+    console.log(err);
+    res.status(404).json({ success: false, data: { error: err } });
+  }
 });
 
-events.get("/:event_id", async (req,res) => {
-    const {id} = req.params;
-    const oneEvent = await getOneEvent(id);
-    try{
-        if(oneEvent) {
-            res.json(oneEvent);
-        }
-    } catch(err) {
-        res.status(404).json({success:false,data:{error:err}});
+events.get("/:event_id", async (req, res) => {
+  const { id } = req.params;
+  const oneEvent = await getOneEvent(id);
+  try {
+    if (oneEvent) {
+      res.json(oneEvent);
     }
-})
+  } catch (err) {
+    res.status(404).json({ success: false, data: { error: err } });
+  }
+});
 
-events.post("/", async(req, res) => {
-    try{
-        const createdEvent = await createEvent(req.body)
-        res.json(createdEvent)
-    } catch(err) {
-        res.status(404).json({success:false, data:{error:err}})
-    }
-})
+events.post("/", async (req, res) => {
+  try {
+    const createdEvent = await createEvent(req.body);
+    res.json(createdEvent);
+  } catch (err) {
+    res.status(404).json({ success: false, data: { error: err } });
+  }
+});
 
-events.delete("/:event_id", async (req, res)=>{
-    try{
-        const {id} = req.params;
-        const deletedEvent = await deleteEvent(id);
-        if(deletedEvent) {
-            res.status(200).json({success: true, payload: {data: deletedEvent}})
-        }
-    } catch(err) {
-        res.status(404).json({success: false, data:{error:err}})
+events.delete("/:event_id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedEvent = await deleteEvent(id);
+    if (deletedEvent) {
+      res.status(200).json({ success: true, payload: { data: deletedEvent } });
     }
-})
+  } catch (err) {
+    res.status(404).json({ success: false, data: { error: err } });
+  }
+});
 
 events.put("/:event_id", async (req, res) => {
-    const {id} = req.params;
-    const updatedEvent = await updateEvent(id, req.body);
-    if(updatedEvent.id) {
-        res.status(200).json(updatedEvent)
-    } else res.status(404).json({sucess:false, data:{error: "no event found"}})
+  const { id } = req.params;
+  const updatedEvent = await updateEvent(id, req.body);
+  if (updatedEvent.id) {
+    res.status(200).json(updatedEvent);
+  } else
+    res.status(404).json({ sucess: false, data: { error: "no event found" } });
 });
 
 module.exports = events;
---
+++ queries/events.js
@@ -0,0 +1,69 @@
+const db = require("../db/dbConfig.js");
+
+const getAllEvents = async () => {
+  try {
+    const allEvents = await db.any("SELECT * FROM events");
+    console.log("data retrieved from databse", allEvents);
+    return allEvents;
+  } catch (err) {
+    console.error("Error in getAllEvents");
+  }
+};
+
+const getOneEvent = async (event_id) => {
+  try {
+    const oneEvent = await db.one(
+      "SELECT * FROM events WHERE event_id=$1",
+      event_id
+    );
+    return oneEvent;
+  } catch (error) {
+    return error;
+  }
+};
+
+const createEvent = async (event) => {
+  try {
+    const createdEvent = db.one(
+      "INSER INTO events (eventName , capacity , floor) VALUES($1,$2,$3) RETURNING *",
+      [event.eventName, event.capacity, event.floor]
+    );
+    return createdEvent;
+  } catch (error) {
+    return error;
+  }
+};
+
+const deleteEvent = async (event_id) => {
+  try {
+    const deletedEvent = await db.one(
+      "DELETE FROM events WHERE event_id=$1 RETURNING *",
+      event_id
+    );
+    return deletedEvent;
+  } catch (error) {
+    return error;
+  }
+};
+
+const updateEvent = async (event_id, event) => {
+  try {
+    const { eventName, capacity, floor } = event;
+    const updatedEvent = await db.one(
+      "UPDATE events SET eventName=$1 , capacity=$2, floor=$3 WHERE event_id RETURNING *",
+      [eventName, capacity, floor, id]
+    );
+    return updatedEvent;
+  } catch (err) {
+    return err;
+  }
+};
+
+
+module.exports = {
+    getAllEvents,
+    getOneEvent,
+    deleteEvent,
+    updateEvent,
+    createEvent,
+}
