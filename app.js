483864aff7040b76d9fd9c9b3bbe169c4f279e1e
Erick Tolentino <ericktolentino@pursuit.org>
2024-01-25 17:17:29 -0500

    added

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const eventSpace = require("./controllers/EventSpaceController")
 
 app.use(cors());
 app.use(express.json());
+app.use("/EventsSpace", eventSpace);
 
 app.get("*", (req, res) => {
     res.status(404).json({success: false, data: {error: "page"}})
 })
+
 
 
 module.exports = app;
