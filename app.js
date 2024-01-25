389b98ceea9e81e9f0442c5137d3e5604eeb26e3
Erick Tolentino <ericktolentino@pursuit.org>
2024-01-25 16:46:41 -0500

    added basic server foundation

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-console.log("hello");
-console.log("Erick")
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("*", (req, res) => {
+    res.status(404).json({success: false, data: {error: "page"}})
+})
+
+module.exports = app;
